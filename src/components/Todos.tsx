1f890400896a60b261e096c61902b1bb5548ee7e
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-09-19 20:34:00 +0530

    inital commit

---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Todos() {
+  return (
+    <div>
+      <ul>
+        <li>Todo 1</li>
+        <li>todo 2</li>
+      </ul>
+    </div>
+  );
+}
